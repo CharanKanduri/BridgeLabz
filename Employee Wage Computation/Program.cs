d291da406f62af1996065b27e83d8d2c32608650
charan_kanduri <charan2000kanduri@gmail.com>
2021-06-28 19:30:32 +0530

    [CHARAN] UC-5_monthlyWageComputation

--- Employee Wage Computation/Program.cs
+++ Employee Wage Computation/Program.cs
@@ -15,6 +15,10 @@ namespace Employee_Wage_Computation
             int employeeAbsent = 0;
             int employeePartTime = 2;
             int employeeStatus;
+            int day = 1;
+            int monthlyWage = 0;
+            int maximumDays = 20;
+
             int wagePerHour = 20;
             Random random = new Random();
             employeeStatus = random.Next(0, 3);
@@ -22,15 +26,24 @@ namespace Employee_Wage_Computation
             switch (employeeStatus)
             {
                 case 1:
-                    Console.WriteLine("Employee Present");
-                    Console.WriteLine("Total wage is: " + wagePerHour * 8);
+                    Console.WriteLine("Full time- Employee Present");
+                    for (day = 1; day <=maximumDays; day++)
+                    {
+                        monthlyWage= monthlyWage +(wagePerHour*8);
+                    }
+                    Console.WriteLine("Total wage for month is:"+monthlyWage);
+
                     break;
                 case 0:
                     Console.WriteLine("Employee Absent");
                     break;
                 case 2:
-                    Console.WriteLine("Employee works part-time");
-                    Console.WriteLine("Total wage is: " + wagePerHour * 4);
+                    Console.WriteLine("part time- Employee Present");
+                    for (day = 1; day <= maximumDays; day++)
+                    {
+                        monthlyWage = monthlyWage + (wagePerHour * 4);
+                    }
+                    Console.WriteLine("Total wage for month is: " + monthlyWage);
                     break;
             }
 
