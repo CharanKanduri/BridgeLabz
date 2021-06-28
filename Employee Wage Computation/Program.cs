0c0254d38ecc54f0c3485475532e4b59a3f7cc5f
charan_kanduri <charan2000kanduri@gmail.com>
2021-06-28 16:49:03 +0530

    [CHARAN] UC-1_EmployeeAttendanceUC

--- Employee Wage Computation/Program.cs
+++ Employee Wage Computation/Program.cs
@@ -7,6 +7,25 @@ namespace Employee_Wage_Computation
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION");
+
+
+            //U1: Check Employee is Present or Absent using Random
+
+            int employeePresent = 1;
+            int employeeAbsent = 0;
+            int employeeStatus;
+            Random random = new Random();
+            employeeStatus = random.Next(0, 2);
+
+            if (employeeStatus == employeePresent)
+            {
+                Console.WriteLine("Employee Present");
+            }
+            else 
+            {
+                Console.WriteLine("Employee Absent");
+            }
+
         }
     }
 }
